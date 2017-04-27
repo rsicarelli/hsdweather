8275e4c730ea9476de02596a7fd0a86d2c5dcd83
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2017-04-27 01:10:49 -0300

    First commit :art:

---
+++ app/src/main/java/br/com/rsicarelli/hsdweather/presentation/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rsicarelli.hsdweather.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.rsicarelli.hsdweather.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
